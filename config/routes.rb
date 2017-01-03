11ea6e3dacaa26934087feed10549f350a825aeb
Ilaria Varriale <ilariasvarriale@gmail.com>
2017-01-03 15:17:33 -0800

    Adds custom route for updating attributes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  resources :tasks
+  resources :tasks do
+    member do
+      put :update_attributes
+    end
+  end
 
   root 'pages#home'
 
